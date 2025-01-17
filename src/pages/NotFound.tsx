71d455931c419c56000ba5a47f5864c4c0cc2eea
Randy Bravo <rbravoh99@gmail.com>
2025-01-16 20:13:24 -0500

    Design changes to not found

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,16 +1,19 @@
 import { Typography, Box } from "@mui/material";
-import { Warning } from "@mui/icons-material";
+import { WarningRounded } from "@mui/icons-material";
 
 const NotFound = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+    <div className="min-h-screen flex mt-56 justify-center bg-gray-50">
       <Box className="text-center space-y-4">
         <div className="flex items-center justify-center gap-2">
-          <Warning sx={{ fontSize: "5rem" }} className="text-sky-500" />
+          <WarningRounded
+            sx={{ fontSize: "12rem" }}
+            className="text-sky-500 font-bold"
+          />
           <Typography
             variant="h1"
             component="span"
-            className="text-6xl font-normal text-sky-500"
+            className="text-9xl !font-extrabold text-sky-500"
           >
             404
           </Typography>
@@ -18,7 +21,7 @@ const NotFound = () => {
         <Typography
           variant="h4"
           component="h1"
-          className="text-gray-600 font-normal"
+          className="text-gray-600 !font-bold"
         >
           Oops... Page Not Found!
         </Typography>
